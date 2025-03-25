0fec9e63ad96580a16c02eb4c8695b5b5ac58af3
lilnerdwithpc <csasdwwewadwa2@gmail.com>
2025-03-25 11:46:53 +0700

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import math
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QPainter, QColor, QVector2D, QVector3D
+from PyQt5.QtGui import QPainter, QColor, QVector2D, QVector3D, QQuaternion
 from PyQt5.QtCore import Qt, QTimer, QPoint, QPointF
 
 idk = 0  # Global variable for animation
@@ -9,6 +9,115 @@ idk = 0  # Global variable for animation
 args = {
     'physics_fps': 60
 }
+
+class RigElement:
+    def __init__(self, name:str, c0:QVector3D=None, c1:QVector3D=None, rotation:QQuaternion=None):
+        self.name = name
+        self.c0 = c0 if c0 is not None else QVector3D()
+        self.c1 = c1 if c1 is not None else QVector3D()
+        self.rotation = rotation if rotation is not None else QQuaternion()
+
+        self._world_position = QVector3D()
+        self.parent = None
+        self.childs = []
+
+    def add_child(self, child:'RigElement'):
+        # TODO: add_child should only be performed once for each child. else it malfunctions, and i wont fix it. why? because yes. why todo? because i might fix it sometime.
+        self.childs.append(child)
+        child.parent = self
+
+    def add_childs(self, childs:list['RigElement']):
+        # TODO: add_childs should only be performed once for each child. else it malfunctions, and i wont fix it. why? because yes. why todo? because i might fix it sometime.
+        for child in childs:
+            self.childs.append(child)
+            child.parent = self
+
+class Rig:
+    def __init__(self, pos:QVector2D):
+        '''
+        - Root
+            - UpperTorso
+                - Head
+                - LeftUpperArm
+                    - LeftLowerArm
+                        - LeftHand
+                - RightUpperArm
+                    - RightLowerArm
+                        - RightHand
+                - LowerTorso
+                    - LeftUpperLeg
+                        - LeftLowerLeg
+                            - LeftFoot
+                    - RightUpperLeg
+                        - RightLowerLeg
+                            - RightFoot
+        '''
+        # Create the erm rig elements
+        self.Root = RigElement('Root')
+
+        self.UpperTorso = RigElement('UpperTorso')
+        self.LowerTorso = RigElement('LowerTorso')
+
+        self.Head = RigElement('Head')
+
+        self.LeftUpperArm = RigElement('LeftUpperArm')
+        self.LeftLowerArm = RigElement('LeftLowerArm')
+        self.LeftHand = RigElement('LeftHand')
+
+        self.RightUpperArm = RigElement('RightUpperArm')
+        self.RightLowerArm = RigElement('RightLowerArm')
+        self.RightHand = RigElement('RightHand')
+
+        self.LeftUpperLeg = RigElement('LeftUpperLeg')
+        self.LeftLowerLeg = RigElement('LeftLowerLeg')
+        self.LeftFoot = RigElement('LeftFoot')
+
+        self.RightUpperLeg = RigElement('RightUpperLeg')
+        self.RightLowerLeg = RigElement('RightLowerLeg')
+        self.RightFoot = RigElement('RightFoot')
+
+        # Connect the rig elements
+        self.Root.add_child(self.UpperTorso)
+        
+        self.UpperTorso.add_childs((self.Head, self.LeftUpperArm, self.RightUpperArm, self.LowerTorso))
+        self.LowerTorso.add_childs((self.LeftUpperLeg, self.RightUpperLeg))
+
+        self.LeftUpperArm.add_child(self.LeftLowerArm)
+        self.RightUpperArm.add_child(self.RightLowerArm)
+
+        self.LeftLowerArm.add_child(self.LeftHand)
+        self.RightLowerArm.add_child(self.RightHand)
+
+        self.RightUpperLeg.add_child(self.RightLowerLeg)
+        self.LeftUpperLeg.add_child(self.LeftLowerLeg)
+        
+        self.RightLowerLeg.add_child(self.RightFoot)
+        self.LeftLowerLeg.add_child(self.LeftFoot)
+
+    def get_layout(self):
+        layout:dict[RigElement,dict] = {self.Root : {}}
+        def _recurve(rig_element:RigElement, childs_dict:dict):
+            for child in rig_element.childs:
+                childs_dict[child] = {}
+                _recurve(child, childs_dict[child])
+        _recurve(self.Root, layout[self.Root])
+        return layout
+
+    def get_layout_string(self):
+        layout = self.get_layout()
+        layout_str = ''
+        def _recurve(layout_sect:dict[RigElement,dict], depth:int):
+            nonlocal layout_str
+            for k, v in layout_sect.items():
+                layout_str += '|   '*depth + k.name + '\n'
+                _recurve(v, depth+1)
+        _recurve(layout, 0)
+        return layout_str
+
+    
+
+rig = Rig(QVector2D())
+print(rig.get_layout_string())
 
 class DesktopGalaxiary(QWidget):
     def __init__(self, args):
@@ -67,7 +176,8 @@ class DesktopGalaxiary(QWidget):
         global idk
         painter.setBrush(QColor(255, 221, 158, 255))
         _draw_joint(QPoint(100, 200), QPoint(300+idk, 300), 30)
-   
+    
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
